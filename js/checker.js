51634b42abb45ca82aa9252110dc685780dd3cef
ThomasTienLun <maxpayne216@gmail.com>
2014-05-17 16:03:00 +0800

    HTM23715-hw4-0517
    0517完成＿Due0518

--- js/checker.js
+++ js/checker.js
@@ -8,25 +8,33 @@ var junkGroups = [];
 
 // 用 Ajax 自 http://spamgroup.tonyq.org/groups/jsonp 取得垃圾社團列表
 
-$.ajax('http://spamgroup.tonyq.org/groups/jsonp', {
-  dataType: 'jsonp',
-  jsonp: 'jsonp',
-  success: function(data){
-    // 將每筆資料的 GID 放進 junkGroups 陣列中。
-    //
-    // ...
-    //   junkGroups.push(data[i].GID);
-    // ...
-    //
+$.getJSON('http://jsbin.com/jaziroja/1', {}, function(data){
+  console.log(data);
+  var i;
+    for (i=0 ; i<data.length ; i+=1){
+       junkGroups.push(data[i].GID);
+    }
     startButton.removeAttr('disabled').removeClass('disabled');
   }
+
 });
+
+//$.ajax('http://spamgroup.tonyq.org/groups/jsonp', {
+//  dataType: 'jsonp',
+//  jsonp: 'jsonp',
+//  success: function(data){
+    // 將每筆資料的 GID 放進 junkGroups 陣列中。
+    //
+// });
 
 // 設定 Facebook AppID
 window.fbAsyncInit = function(){
   FB.init({
-    appId: '259195264246285', // 若可以，請換成自己的 App ID !
-    status: true
+    appId: '305282539596533', // 若可以，請換成自己的 App ID !
+    status: true,
+    xfbml: true,
+    version: 'v2.0',
+
   });
 
   // 比對每個使用者的 group 是否有在 junkGroups 中出現
@@ -36,9 +44,32 @@ window.fbAsyncInit = function(){
     $('.hw4-complete').remove(); // 移除「掃描完成」
 
     // 1. 讓使用者登入此 Facebook App (FB.login)
+    FB.login(function(){
+      console.log('Logged in!');
+      FB.api('/me/groups', function(resp){
+        console.log('User groups:', resp.data);
+        var i;
+        for( i=0; i<resp.data.length; i+=1){
+          group = resp.data[i];
+          if( junkGroups.indexOf(group.id) !== -1){
+            results.append('<tr><td>'+group.id+'</td><td>'+group.name+'</td></tr>')  
+          }
+        }
+        results.after('<div class="hw4-complete alert alert-info">掃描完成</div>');
+      });
+    },{scope: 'user_groups'});
     // 2. 以 FB.api 拿到使用者的 group 列表
     // 拿到使用者 group 列表的 response 之後：
-    // results.after('<div class="hw4-complete alert alert-info">掃描完成</div>');
+    FB.api(
+    //"/me/groups",
+    function (response) {
+      if (response && !response.error) {
+        /* handle the result */
+      }
+    }
+);
+
+
 
   });
 };
